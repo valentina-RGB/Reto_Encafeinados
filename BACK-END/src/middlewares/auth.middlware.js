5d4cdb07e33501957fe7d33b3bc3ed0c12ab8711
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-01-22 16:37:50 -0500

    JsonWebToken

---
+++ BACK-END/src/middlewares/auth.middlware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({ message: 'Token no proporcionado' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Token inválido o expirado', error: error.message });
+    }
+};
+
+module.exports = {
+    authenticateJWT
+};
