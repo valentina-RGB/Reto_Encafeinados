b8f96489a2d8535174319036db3afa9f9e39cd99
Jose David Henao Camacho <jose_dhenaoc@soy.sena.edu.co>
2025-01-24 10:27:55 -0500

    DETALLES

--- FRONT-END/src/components/layout/layout.tsx
+++ FRONT-END/src/components/layout/layout.tsx
@@ -12,51 +12,40 @@ const Layout = () => {
 
   return (
     //import Image from "next/image"
-    <div>
-       <div className="h-screen flex flex-col  bg-[#6F4E37]">
-      <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f3ece7] px-10 py-3">
-      <div className="flex items-center gap-4 text-[#1b130d]">
-          {/* Botón de regresar */}
-         <Button 
-          icon="pi pi-arrow-left" 
-          className="p-button-text text-[#5c412d] hover:bg-[#ebdfd6]" 
-          onClick={() => navigate('/menuStore')} 
-        />
-        
-        {/* Logo */}
-        <h4>Encafeinados</h4>
-        {/* <img 
-          src="/logo-name.JPG" 
-          alt="Encafeinados" 
-          className="h-8 object-contain" 
-        /> */}
-        
-        {/* Botones de usuario y cerrar sesión */}
-        <div className="flex items-center gap-4">
-          {/* <Button 
-         <div className="flex items-center gap-4">
-           <Button 
-            icon="pi pi-user" 
-            className="p-button-text text-white hover:bg-[#D2691E]" 
-            onClick={() => console.log("Abrir perfil")} 
-          /> */}
-          <Button 
-            icon="pi pi-sign-out" 
-            className="p-button-text text-white hover:bg-[#F4A460]" 
-            onClick={handleLogout} 
-          />
-        </div>
-      </div>
-    </header>
-      {/* Main Content */}
-      <main className="mt-0 mb-2  flex-1 overflow-y-auto">
-       <div className="p-4 bg-[#F5F5F5]  shadow-md h-full">
-         {/* Contenido principal */}
-         <Outlet />
-       </div>
-     </main>
+    <div className="h-screen flex flex-col bg-[#6F4E37]">
+  <header className="flex items-center justify-between px-10 py-3 border-b border-solid border-b-[#f3ece7]">
+    <div className="flex items-center gap-4 text-[#1b130d]">
+      {/* Botón de regresar */}
+      <Button 
+        icon="pi pi-arrow-left" 
+        className="p-button-text text-[#5c412d] hover:bg-[#ebdfd6]" 
+        onClick={() => navigate('/menuStore')} 
+      />
+      
+      {/* Logo */}
+      <h4 className="text-xl font-semibold">Encafeinados</h4>
+      
     </div>
+    
+    {/* Botones de usuario y cerrar sesión */}
+    <div className="flex items-center gap-4">
+      <Button 
+        icon="pi pi-sign-out" 
+        className="p-button-text text-white hover:bg-[#F4A460]" 
+        onClick={handleLogout} 
+      />
     </div>
+  </header>
+  
+  {/* Main Content */}
+  <main className="mt-0 mb-2 flex-1 overflow-y-auto">
+    <div className="p-4 bg-[#F5F5F5] shadow-md h-full">
+      {/* Contenido principal */}
+      <Outlet />
+    </div>
+  </main>
+</div>
+
    
     
   
